d4d71c043c584d57af2eae4720484c5256a225d6
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-06-25 14:40:57 +0900

    Improve: FoodモデルをSTIするため

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :italian_foods
+  resources :chinese_foods
+  resources :japanese_foods
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # resources :exercises, only: [] do
   resources :exercises do
